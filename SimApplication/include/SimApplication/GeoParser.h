26624dad9596a7b86b75349ecb98a258d622fe87
omar-moreno <email@omarmoreno.net>
2019-10-16 16:44:50 -0700

    Interface for parser used to build and load geometry into Geant4.

---
+++ SimApplication/include/SimApplication/GeoParser.h
@@ -0,0 +1,29 @@
+
+#ifndef _SIM_APPLICATION_GEO_PARSER_H_
+#define _SIM_APPLICATION_GEO_PARSER_H_
+
+/*    Geant4    */
+#include "G4VPhysicalVolume.hh"
+
+namespace ldmx { 
+
+    /**
+     * Interface for parser used to load geometry into Geant4. 
+     */
+    class GeoParser {
+       
+        public:  
+        
+            /** Destructor */
+            virtual ~GeoParser() {}
+
+            /** 
+             * Pure virtual method used to retrieve the world volume after parsing.
+             *
+             * @return The world volume 
+             */
+            G4VPhysicalVolume* GetWorldVolume() = 0; 
+    };
+}
+
+#endif // _SIM_APPLICATION_GEO_PARSER_H_
