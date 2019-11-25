9a60464768c1832ba26abc06a7bd29b5577c77ac
Andrew Whitbeck <whitbeck.andrew@gmail.com>
2019-11-25 07:50:57 -0800

    updating number of cells

--- EventProc/python/trigScintDigis.py
+++ EventProc/python/trigScintDigis.py
@@ -5,8 +5,8 @@ from LDMX.Framework import ldmxcfg
 trigScintDigis = ldmxcfg.Producer("trigScintDigis", "ldmx::TrigScintDigiProducer")
 
 trigScintDigis.parameters["meanNoise"] = 0.02
-trigScintDigis.parameters["number_of_strips"] = 50
-trigScintDigis.parameters["number_of_arrays"] = 3
+trigScintDigis.parameters["number_of_strips"] = 62
+trigScintDigis.parameters["number_of_arrays"] = 1
 trigScintDigis.parameters["mev_per_mip"] = 0.4
 trigScintDigis.parameters["pe_per_mip"] = 10.
 trigScintDigis.parameters["input_collection"]="TriggerPadUpSimHits"
@@ -15,8 +15,8 @@ trigScintDigis.parameters["output_collection"]="trigScintDigis"
 trigScintDigisDn = ldmxcfg.Producer("trigScintDigis", "ldmx::TrigScintDigiProducer")
 
 trigScintDigisDn.parameters["meanNoise"] = 0.02
-trigScintDigisDn.parameters["number_of_strips"] = 50
-trigScintDigisDn.parameters["number_of_arrays"] = 3
+trigScintDigisDn.parameters["number_of_strips"] = 62
+trigScintDigisDn.parameters["number_of_arrays"] = 1
 trigScintDigisDn.parameters["mev_per_mip"] = 0.4
 trigScintDigisDn.parameters["pe_per_mip"] = 10.
 trigScintDigisDn.parameters["input_collection"]="TriggerPadDownSimHits"
@@ -25,8 +25,8 @@ trigScintDigisDn.parameters["output_collection"]="trigScintDigisDn"
 trigScintDigisTag = ldmxcfg.Producer("trigScintDigis", "ldmx::TrigScintDigiProducer")
 
 trigScintDigisTag.parameters["meanNoise"] = 0.02
-trigScintDigisTag.parameters["number_of_strips"] = 50
-trigScintDigisTag.parameters["number_of_arrays"] = 3
+trigScintDigisTag.parameters["number_of_strips"] = 62
+trigScintDigisTag.parameters["number_of_arrays"] = 1
 trigScintDigisTag.parameters["mev_per_mip"] = 0.4
 trigScintDigisTag.parameters["pe_per_mip"] = 10.
 trigScintDigisTag.parameters["input_collection"]="TriggerPadTaggerSimHits"
